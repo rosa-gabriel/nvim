e85ae16909e17b8bbcdfa70e40473834616d9667
GabrielWaif <gabrielrosarcg@hotmail.com>
2023-04-09 16:49:29 -0300

    Added keybinds

--- lua/options/remap.lua
+++ lua/options/remap.lua
@@ -7,7 +7,13 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) --Open vim file explorer
 --vim.keymap.set("v", "<leader>y", "\"+y");
 --vim.keymap.set("n", "<leader>Y", "\"+Y");
 
+-- Move up and down on edit mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.keymap.set("x", "<leader>p", "\"_dP") -- Paste without yanking 
+
+vim.keymap.set("i", "<C-p>", "<Nop>") -- Cntrl-p is useless
 
 vim.keymap.set("i", "<C-H>", "<C-w>") -- Windows bind 
 vim.keymap.set("i", "<C-BS>", "<C-w>") -- wsl <Cntrl-BackSpace> bind
