d40b4027b202b7dfea8a41342f896a803123e0b4
Gabriel Rosa <gabrielrosarcg@hotmail.com>
2023-04-21 17:49:59 -0300

    java works fuckeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeer

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,7 @@
 local lsp = require("lsp-zero")
 
+local system = "windows";
+
 lsp.preset("recommended") -- Chooses the default settings for LSP
 
 lsp.ensure_installed({
@@ -56,6 +58,44 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
 end)
+
+if system == "windows" then
+	local user = 'gabri'
+	local workspace_dir = 'G:/src/java/'
+	local jdtls_path = 'C:/Users/'..user..'/AppData/Local/nvim-data/mason/share/jdtls'
+	local path_to_lsp_server = "C:/Users/"..user.."/AppData/Local/nvim-data/mason/packages/jdtls/config_win"
+	local path_to_plugins = jdtls_path .. "/plugins/"
+	local path_to_jar = path_to_plugins .. "org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar"
+
+	local root_markers = { "mvnw", "gradlew", "pom.xml", "build.gradle" }
+	local root_dir = require("jdtls.setup").find_root(root_markers);
+
+	local java_root_finder = (function()
+		return root_dir;
+	end)
+
+	lsp.configure('jdtls',
+		{
+			cmd = {
+				'java',
+				'-Declipse.application=org.eclipse.jdt.ls.core.id1',
+				'-Dosgi.bundles.defaultStartLevel=4',
+				'-Declipse.product=org.eclipse.jdt.ls.core.product',
+				'-Dlog.protocol=true',
+				'-Dlog.level=ALL',
+				'-Xms1g',
+				'--add-modules=ALL-SYSTEM',
+				'--add-opens', 'java.base/java.util=ALL-UNNAMED',
+				'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+				'-jar', path_to_jar,
+				'-configuration', path_to_lsp_server,
+				'-data', workspace_dir,
+			},
+			root_dir = java_root_finder,
+		}
+	) 
+else end
 
 lsp.setup()
 
