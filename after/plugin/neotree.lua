b3d4bab57a88c2dabe97332ffe21441b0a922d73
Gabriel Rosa <gabrielrosarcg@hotmail.com>
2023-04-22 19:29:51 -0300

    neotree

---
+++ after/plugin/neotree.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<cr>", {}) -- Goes to file at index(1) in harpoon buffer
--- lua/options/packer.lua
+++ lua/options/packer.lua
@@ -6,6 +6,15 @@ return require('packer').startup(function(use)
 
 	-- UI
 	use { "rebelot/kanagawa.nvim" } -- Theme
+	use {
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+	}
 	use 'nvim-tree/nvim-web-devicons' -- icons
 
 	use {
@@ -62,12 +71,3 @@ return require('packer').startup(function(use)
 
 end);
 
-	-- use {
-	-- 	"nvim-neo-tree/neo-tree.nvim",
-	-- 	branch = "v2.x",
-	-- 	requires = { 
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-	-- 		"MunifTanjim/nui.nvim",
-	-- 	}
-	-- }use 'nvim-tree/nvim-web-devicons' -- icons
