3b8a6008721045203dc2dcb1276a4804cd1035b8
Gabriel Eduardo da Silva Rosa <e-geduardo@weg.net>
2023-09-25 07:23:55 -0300

    Neorg

--- lua/options/packer.lua
+++ lua/options/packer.lua
@@ -98,4 +98,26 @@ return require('packer').startup(function(use)
             require('Comment').setup()
         end
     }
+
+    -- Neorg
+    use {
+        "nvim-neorg/neorg",
+        config = function()
+            require('neorg').setup {
+                load = {
+                    ["core.defaults"] = {}, -- Loads default behaviour
+                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                    ["core.dirman"] = {  -- Manages Neorg workspaces
+                        config = {
+                            workspaces = {
+                                notes = "~/notes",
+                            },
+                        },
+                    },
+                },
+            }
+        end,
+        run = ":Neorg sync-parsers",
+        requires = "nvim-lua/plenary.nvim",
+    }
 end);
