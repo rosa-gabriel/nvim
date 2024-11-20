181530509c2ef11783b691a11a224292245f3f73
Gabriel Eduardo Da Silva Rosa - External <e-geduardo@weg.net>
2024-11-20 10:22:49 -0300

    update to use lsp-zero 3.*

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,49 +9,46 @@ return {
         'williamboman/mason-lspconfig.nvim',
     },
     config = function()
-        local lsp = require("lsp-zero")
-        local cmp = require('cmp')
+        local lsp_zero = require('lsp-zero')
+
+        lsp_zero.on_attach(function(_, bufnr)
+          local opts = {buffer = bufnr, remap = false}
+
+          vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+          vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+          vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+          vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+          vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+          vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+          vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+          vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+          vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+          vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+          vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+        end)
+
         require('mason').setup({})
         require('mason-lspconfig').setup({
-            handlers = {
-                lsp.default_setup,
-            },
+          handlers = {
+            lsp_zero.default_setup,
+            lua_ls = function()
+              local lua_opts = lsp_zero.nvim_lua_ls()
+              require('lspconfig').lua_ls.setup(lua_opts)
+            end,
+          }
         })
 
-        lsp.preset("recommended")
-        lsp.set_preferences({
-            suggest_lsp_servers = false,
-            sign_icons = {
-                error = 'E',
-                warn = 'W',
-                hint = 'H',
-                info = 'I'
-            }
-        })
-        lsp.on_attach(function(_, bufnr)
-            local opts = { buffer = bufnr, remap = false }
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-        end)
-        lsp.setup()
+        local cmp = require('cmp')
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-        local cmp_mappings = lsp.defaults.cmp_mappings({
-            ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),     -- Goes to next option on LSP
-            ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),     -- Goes to next option on LSP
-            ['<C-y>'] = cmp.mapping.confirm({ select = true }),       -- Goes to confirms option in LSP
-            ['<C-Space>'] = cmp.mapping.complete(),
-        })
         cmp.setup({
-            mapping = cmp_mappings
+          sources = {
+            {name = 'path'},
+            {name = 'nvim_lsp'},
+            {name = 'nvim_lua'},
+          },
+          mapping = cmp.mapping.preset.insert({
+            ['<C-Space>'] = cmp.mapping.complete(),
+          }),
         })
     end
 }
