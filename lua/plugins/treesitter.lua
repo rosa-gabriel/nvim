badbf9e35e3607525bfb1b434cfe3f4817ff3b21
Gabriel Eduardo da Silva Rosa <e-geduardo@weg.net>
2024-11-20 14:18:47 -0300

    switching clang for gcc

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = (function()
-        require 'nvim-treesitter.install'.compilers = { "clang" }
+        require 'nvim-treesitter.install'.compilers = { "gcc" }
         require 'nvim-treesitter.configs'.setup {
             sync_install = false,
             auto_install = true,
